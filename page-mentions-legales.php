8377871b4930dd6dcb1bc91b9dabbd6b2aed4ad4
Unknown <fantomette.37@hotmail.fr>
2018-05-16 14:30:02 +0200

    Afficher le contenu de la page

--- page-mentions-legales.php
+++ page-mentions-legales.php
@@ -14,8 +14,13 @@
 
 <?php get_header(); ?>
 
-<h1><?php the_title();?></h1>
-<p><?php the_content();?></p>
+
+<?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+		<h1><?php the_title();?></h1>
+        <p><?php the_content();?></p>				
+                        
+<?php endwhile; ?>
+                    <?php endif; ?>
 
 
 <?php get_footer(); ?>
