b4beb5cc73a5ce4493f484fdebc04fe9b854e492
Unknown <fantomette.37@hotmail.fr>
2018-05-16 13:35:11 +0200

    Modification de page.php

---
+++ page.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+ *
+ * Template Name: Template Page Normale
+ *
+ * (You can also use other lines before or after the line above,
+ *  WordPress only cares about the line that starts with "Template Name".)
+ *
+ */
+
+?>
+
+
+<?php get_header(); ?>
+
+<h1><?php the_title();?></h1>
+<p><?php the_content();?></p>
+
+
+<?php get_footer(); ?>
