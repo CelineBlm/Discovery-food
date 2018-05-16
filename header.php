e5d67a71e92144f15a5c3a26995d98a44b2bc76f
Unknown <celine.bellamy@gmail.com>
2018-05-16 14:54:30 +0200

    Essai d'intégration slider non concluant

--- header.php
+++ header.php
@@ -18,6 +18,28 @@
                     </ul>
 		<!-- conatiner -->
 		
+		
+<div class="slider js_slider">
+    <div class="frame js_frame">
+        <ul class="slides js_slides">
+           
+           <?php $the_query = new WP_Query( array( 'post_type' => 'recette' ) ); ?>
+              <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+           
+            <li class="js_slide"><?php the_post_thumbnail('thumbnail');?></li>
+            
+            <?php endwhile; ?>
+           <?php endif; ?>
+        </ul>
+    </div>
+    <span class="js_prev prev">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5"><g><path fill="#2E435A" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"/></g></svg>
+    </span>
+    <span class="js_next next">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5"><g><path fill="#2E435A" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z"/></g></svg>
+    </span>
+</div>
+		
 
 
 
