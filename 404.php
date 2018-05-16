5bb2ea5fd30a588f7e6e8b4575d4e3b612991df3
Unknown <fantomette.37@hotmail.fr>
2018-05-16 14:41:09 +0200

    Page 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+
+<?php get_footer(); ?>
+
+
+<iframe src="https://giphy.com/embed/eSQKNSmg07dHq" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<p>Sorry Bro, y a aucune recette qui correspond à tes attentes</p>
+<a href="index.php">Retour à l'accueil</a>
