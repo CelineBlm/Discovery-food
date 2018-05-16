7fec16f62ce456b4695aa1461b3ce9f4a64b9795
Unknown <fantomette.37@hotmail.fr>
2018-05-16 13:13:09 +0200

    Création d'un template de page à propos

---
+++ page-a-propos.php
@@ -0,0 +1,12 @@
+<?php
+ 
+/*
+ *
+ * Template Name: Template A Propos
+ *
+ * (You can also use other lines before or after the line above,
+ *  WordPress only cares about the line that starts with "Template Name".)
+ *
+ */
+ 
+?>
