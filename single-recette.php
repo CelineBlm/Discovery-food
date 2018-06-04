2cef8592a812691725ef9a544a9d1db1e38b6f79
Unknown <celine.bellamy@gmail.com>
2018-06-04 16:08:21 +0200

    Modification du css

--- single-recette.php
+++ single-recette.php
@@ -9,54 +9,63 @@
         <div id="single-recipe">
         <div class="container">
 			<div class="row">
-       <div class="col-md-6 col-lg-8 col-md-12">
+       <div class="content">
         <h1><?php the_title(); ?> </h1>
-            <div class="recipe">
+            <div class="recipe main">
                 
                 <?php the_post_thumbnail('medium', array('class' => 'img.responsive')) ?>
                 
                 
-            </div>
+            
 
         
             
-            <div class="col-md-6 col-lg-4 col-md-12">
-       
-            <p><?php the_content(); ?> </p>
-                
-            </div>
-                </div></div>
-        
-      </div>  
-                </div>
-                
+            
+
+<div class="aside_recipe">   
+    <div id="info_nutri">          
 <h2>Valeurs nutritionnelles</h2>
 <p><?php the_field('field_calories');?> cal</p>
 <p><?php the_field('field_glucides');?> g</p>
 <p><?php the_field('field_proteines');?> g</p>
 <p><?php the_field('field_lipides');?> g</p>
+</div>
+
+<div id="time">
 
 <h2>Temps total pour cuisiner <?php the_title(); ?></h2>
 <p><?php the_field('field_temps_prepa');?> minutes</p>
 <p><?php the_field('field_temps_cuisson');?> minutes</p>
+ </div>
+           </div>
+           
+           <div class="recipe_details">
+       
+            <p><?php the_content(); ?> </p>
+                
+            </div> 
+           
+           </div>
+        
+  </div>    
 
- <aside id="sidebar" class="col-md-6 col-lg-4 col-md-12">
+ <aside id="sidebar">
                    <ul>
                        <li>
                          <?php dynamic_sidebar( 'right-sidebar' ); ?>  
                        </li>
                    </ul>
                     
-            
+  </aside>
+              
 
 <?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
-        
-</div>
-            </div>
-            </div>
-       </div>
+ 
+      </div>
+       </div>  
+                </div>
        
     
 <?php get_footer(); ?>
