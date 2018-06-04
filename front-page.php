c5519d54ecd3db1f3f34c164efe04ad170157209
Unknown <celine.bellamy@gmail.com>
2018-06-04 15:38:11 +0200

    J'ai tout cassé le css :)

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 		<div class="container">
 			<div class="row">
 
-				<div class="col-md-6 col-lg-8 col-md-12">
+				<div class="content ">
 					<div class="blog-posts">
                            <?php $the_query = new WP_Query( array( 'post_type' => 'recette' ) ); ?>
                                 <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
@@ -17,7 +17,7 @@
 								</div>
 							</div>
 							<p class="date"><em><?php the_date('d M Y'); ?></em></p>
-                            <h3 class="title"><a href="#"><b class="light-color"><?php the_title() ?></b></a></h3>
+                            <h3 class="title"><a href="<?php echo get_permalink(); ?>"><b class="light-color"><?php the_title() ?></b></a></h3>
                                     <p><?php the_excerpt() ?></p>
 
                             <a class="btn read-more-btn" href="<?php echo get_permalink(); ?>">Lire la recette</a>
@@ -31,7 +31,7 @@
                     </div>
                 </div>
                
-                   <aside id="sidebar" class="col-md-6 col-lg-4 col-md-12">
+                   <aside id="sidebar">
                    <ul>
                        <li>
                          <?php dynamic_sidebar( 'right-sidebar' ); ?>  
