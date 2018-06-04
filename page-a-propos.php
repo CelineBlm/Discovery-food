9e5a69478fe08f7940c1d1878599736965b35b1c
Unknown <fantomette.37@hotmail.fr>
2018-06-04 16:27:15 +0200

    Template page à propos

--- page-a-propos.php
+++ page-a-propos.php
@@ -10,3 +10,17 @@
  */
  
 ?>
+
+
+<?php get_header(); ?>
+
+
+<?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+		<h1 class="a-propos-de-nous"><?php the_title();?></h1>
+        <p class="a-propos-de-nous"><?php the_content();?></p>				
+                        
+<?php endwhile; ?>
+                    <?php endif; ?>
+
+
+<?php get_footer(); ?>
